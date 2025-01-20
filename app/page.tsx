2ca3c82d56911960c9393a97744cd9bec4c222bd
lacerda <edulacerdaaa@gmail.com>
2025-01-20 19:26:58 -0300

    update: adicionando seção de preços e componente de explicação em vídeo

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,17 @@
 import Image from "next/image";
 import Hero from "./components/landing-page/hero";
 import Header from "./components/landing-page/header";
+import VideoExplanation from "./components/landing-page/video-explanation";
+import Pricing from "./components/landing-page/pricing";
 
 export default function Home() {
   return (
     <div className="max-w-7xl mx-auto ">
       <Header/>
       <Hero/>
-      {/* 
       <VideoExplanation/>
       <Pricing/>
+      {/* 
       <FAQ/> */}
     </div>
   );
